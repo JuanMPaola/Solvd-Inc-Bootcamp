f9281e279a90f4f9450887bed55d0b20ecfd07a8
JuanMPaola <juanmpaola25@gmail.com>
2024-04-29 16:51:52 -0300

    Homework5: 4/5 Taks done

--- Homework/5-Arrays.js
+++ Homework/5-Arrays.js
@@ -1,22 +1,97 @@
 /*Task 1: Advanced Array Filtering
-Create a function called customFilterUnique that takes an array and a callback function as arguments. The customFilterUnique function should filter the array using the callback function to determine uniqueness. 
+Create a function called customFilterUnique that takes an array and a callback function as arguments. 
+The customFilterUnique function should filter the array using the callback function to determine uniqueness. 
 The resulting array should contain only unique elements based on the callback's logic.
-Use the customFilterUnique function to filter an array of objects based on a specific property and return only unique objects./*
+Use the customFilterUnique function to filter an array of objects based on a specific property and return only unique objects.*/
 
+const customFilterUnique = (array, cb) =>{
+
+    const filteredArray = [];
+
+    array.forEach(element => {
+        if(cb(element)){
+            if(!filteredArray.includes(element)){
+                filteredArray.push(element);
+            }
+        }
+    });
+    return filteredArray;
+}
 
 
 /*Task 2: Array Chunking
-Create a function called chunkArray that takes an array and a chunk size as arguments. The chunkArray function should divide the array into smaller arrays, each containing elements of the specified chunk size. The function should return an array of arrays.
-Optimize the chunkArray function to minimize memory usage while chunking the array.
+Create a function called chunkArray that takes an array and a chunk size as arguments. 
+The chunkArray function should divide the array into smaller arrays, each containing elements of the specified chunk size. 
+The function should return an array of arrays.
+Optimize the chunkArray function to minimize memory usage while chunking the array.*/
 
-Task 3: Array Shuffling
+const chunkArray = (array, chunk) =>{
+    if(!Array.isArray(array)) return new Error ("First input must be an array");
+    if (typeof chunk !== 'number' || !Number.isInteger(chunk) || chunk <= 0) return new Error("Second input must be a positive integer");
+
+    const bigArray = [];
+    let startIndex = 0;
+    
+    while (startIndex < array.length) {
+        bigArray.push(array.slice(startIndex, startIndex + chunk));
+        startIndex += chunk;
+    }
+
+    return bigArray;
+}
+
+/*Task 3: Array Shuffling
 Create a function called customShuffle that takes an array as an argument and returns a new array with its elements randomly shuffled.
-Implement the customShuffle function using an efficient shuffling algorithm to achieve uniform randomness.
+Implement the customShuffle function using an efficient shuffling algorithm to achieve uniform randomness.*/
 
-Task 4: Array Intersection and Union
+const customShuffle = (array) => {
+    if(!Array.isArray(array)) return new Error ("Input must be an array");
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+/*Task 4: Array Intersection and Union
 Create a function called getArrayIntersection that takes two arrays as arguments and returns a new array containing the common elements between the two arrays.
-Create a function called getArrayUnion that takes two arrays as arguments and returns a new array containing all unique elements from both arrays, without any duplicates.
+Create a function called getArrayUnion that takes two arrays as arguments and returns a new array containing all unique elements from both arrays, without any duplicates.*/
 
-Task 5: Array Performance Analysis
-Implement a function called measureArrayPerformance that takes a function and an array as arguments. The measureArrayPerformance function should execute the provided function with the given array as input and measure the execution time.
+const getArrayIntersection = (array, array2) => {
+    if(!Array.isArray(array) || !Array.isArray(array2)) return new Error ("Inputs must be an array");
+
+    const intersection = [];
+    array.forEach((element)=>{
+        if(array2.includes(element)) intersection.push(element);
+    })
+    return intersection;
+}
+
+const getArrayUnion = (array, array2) => {
+    if(!Array.isArray(array) || !Array.isArray(array2)) return new Error ("Inputs must be an array");
+
+    const union = [];
+    array.forEach((element)=>{
+        if(!array2.includes(element) && !union.includes(element)) union.push(element);
+    })
+    return union;
+}
+
+/*Task 5: Array Performance Analysis
+Implement a function called measureArrayPerformance that takes a function and an array as arguments. 
+The measureArrayPerformance function should execute the provided function with the given array as input and measure the execution time.
 Use the measureArrayPerformance function to compare the performance of built-in array methods (map, filter, reduce, etc.) against your custom array manipulation functions.*/
+
+const measureArrayPerformance = (fn, array) =>{
+
+
+}
+
+module.exports = {
+    task1:{customFilterUnique},
+    task2:{chunkArray},
+    task3:{customShuffle},
+    task4:{getArrayIntersection, getArrayUnion},
+    task5:{}
+}
